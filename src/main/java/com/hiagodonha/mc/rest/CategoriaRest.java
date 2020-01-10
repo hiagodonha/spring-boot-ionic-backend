cf0710bd0482b39d5c964e725f4054e0e33e2738
Hiago Donha <hiagodefenix@gmail.com>
2020-01-09 22:48:33 -0300

    Testando primeira classe de dominio

--- src/main/java/com/hiagodonha/mc/rest/CategoriaRest.java
+++ src/main/java/com/hiagodonha/mc/rest/CategoriaRest.java
@@ -1,16 +1,29 @@
 package com.hiagodonha.mc.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hiagodonha.mc.model.Categoria;
 
  @RestController
  @RequestMapping("categorias")
  public class CategoriaRest {
 	 
 	 @GetMapping
-	 public String listar() {
-		 return "rest está funcionando";
+	 public List<Categoria> listar() {
+		 
+		 Categoria cat1 = new Categoria(1, "Informática");
+		 Categoria cat2 = new Categoria(1, "Escritório");
+		 
+		 List<Categoria> list = new ArrayList<>();
+		 list.add(cat1);
+		 list.add(cat2);
+		 
+		 return list;
 	 }
 	 
 }
