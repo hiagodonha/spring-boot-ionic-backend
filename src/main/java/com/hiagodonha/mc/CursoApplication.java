9c75f10bab010e8182c312701b94d7ec7f8ed376
Hiago Donha <hiagodefenix@gmail.com>
2020-01-11 01:17:04 -0300

    Criando operação de instanciação

--- src/main/java/com/hiagodonha/mc/CursoApplication.java
+++ src/main/java/com/hiagodonha/mc/CursoApplication.java
@@ -1,13 +1,32 @@
 package com.hiagodonha.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hiagodonha.mc.dao.CategoriaDao;
+import com.hiagodonha.mc.model.Categoria;
+
 @SpringBootApplication
-public class CursoApplication {
+public class CursoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaDao categoriaDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaDao.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
